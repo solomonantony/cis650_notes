f4f0b0739dc65cf097f4247f96cdbdb3e1d51be7
Solomon Antony <santony@murraystate.edu>
2025-03-14 12:12:22 -0500

    updated age_test

---
+++ statements_notes/02_age_test.py
@@ -0,0 +1,13 @@
+name = input('What is your name?')
+age  = int(input('What is your age?'))
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+else:
+    print('You are not over the hill')
+    
